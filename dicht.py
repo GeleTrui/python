09be1d9cbe5f8a9611428b504dc7311961a90054
Your Name <danielwillemsen27@gmail.com>
2020-09-25 15:35:01 +0200

    leuk gedicht

---
+++ dicht.py
@@ -0,0 +1,13 @@
+import time 
+
+verse1 = "Mijn naam is daan ik hou van banaan"
+verse2 = "ik hou van burger king, en mijn fietsbel gaat tring"
+verse3 = "ik studeer games, en op je moeder run ik trains."
+verse4 = "ik kan niet echt programeren, maar ben bereid om te leren"
+print(verse1)
+time.sleep(1)
+print(verse2)
+time.sleep(1)
+print(verse3)
+time.sleep(1)
+print(verse4)
---
+++ helloworld.py
@@ -0,0 +1,29 @@
+import time
+
+a = True
+while a == True:
+    hello == "Hello, mijn naam is {owner}. Wat is de jouwe?"
+    print(hello.format(owner = "Daniel"))
+   
+    username = input("Mijn naam is:")
+ if username == Daniel 
+    print("Goededag " + username, "Hier is de datum")
+    else print("tyf op das nii goed")
+    
+
+    import datetime
+
+    x = datetime.datetime.now()
+
+    print("Vandaag is het " + x.strftime("%d"), "van " + x.strftime ("%B"))
+    while True: 
+             query = input('Wil je doorgaan? Y/N')
+             Fl = query[0].lower()
+             if query == '' or not Fl in ['y','n']: 
+                print('Wel antwoorden vriend!') 
+             else: 
+                break 
+    if Fl == 'y': 
+            print("das cool!")
+    if Fl == 'n': 
+            break
