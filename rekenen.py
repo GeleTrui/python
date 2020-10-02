fc4d95c39234d04ac7fe73c5f5802e6328bd5461
Your Name <danielwillemsen27@gmail.com>
2020-10-02 14:55:02 +0200

    rekenen :D

---
+++ rekenen.py
@@ -0,0 +1,21 @@
+even = 10
+print(even)
+plus = 3+1
+print(plus)
+min = 5-1
+print(min)
+delen = 4/2
+print(delen)
+restwaarde= 50%150
+print(restwaarde)
+dwdd = 10
+dwdd += 1
+print(dwdd)
+keer = 4
+keer *= 2
+print(keer)
+delen = 10
+delen /= 5
+print(delen)
+#das toch mooi
+
