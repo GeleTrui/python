7446b2846ff219560c48d8ce1d4bfcf1f6aaede5
Your Name <danielwillemsen27@gmail.com>
2020-10-02 14:32:33 +0200

    das cool

---
+++ pla.py
@@ -0,0 +1,32 @@
+import time
+
+name = "daniel"
+jumpforce = "like 4 lol"
+special_power = "kan pizza bevroren eten"
+speed = "snel, like, 40"
+lengte = "189 cm"
+kennis = "weinig"
+jonko = "geklapt"
+biertje = "op"
+built = "different"
+cool = "True"
+
+print("name: " + name)
+time.sleep(1)
+print("jumpforce: " + jumpforce)
+time.sleep(1)
+print("special power: " + special_power)
+time.sleep(1)
+print("speed: " + speed)
+time.sleep(1)
+print("lengte: " + lengte)
+time.sleep(1)
+print("kennis: " + kennis)
+time.sleep(1)
+print("jonko? " + jonko)
+time.sleep(1)
+print("Biertje? " + biertje)
+time.sleep(1)
+print("built?: " + built)
+time.sleep(1)
+print("cool?: " + cool)
