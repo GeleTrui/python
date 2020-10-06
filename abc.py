27126bf01db19670d2f67b83529d11a840f460be
Your Name <danielwillemsen27@gmail.com>
2020-10-06 12:25:48 +0200

    nice

---
+++ abc.py
@@ -0,0 +1,15 @@
+print("welcome to gaem")
+print("je loopt, ga je links of rechts?")
+print("A: links B: rechts C: rechtdoor")
+optie = input("A, B or C: ")
+if optie == "A":
+   print("je gaat links, wat nu")
+optie2 = input("1: rechts 2: nog een links.")
+if optie2 == "1":
+   print("je gaat rechts")
+elif optie2 == "2":
+   print("je neemt nog een links")
+elif optie == "B":
+   print("je gaat een rechts")
+elif optie == "C"
+   print("je gaat rechtdoor")
