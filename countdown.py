5352f61851266d411e1b7d63020df8d4cb96509e
GeleTrui <70946651+GeleTrui@users.noreply.github.com>
2020-10-29 18:05:58 +0100

    countdown

---
+++ countdown.py
@@ -0,0 +1,21 @@
+countDown = 1000
+while (countDown >= 0):
+    print(countDown)
+    countDown = countDown - 1
+    if countDown == 0:
+        print("""\
+
+
+                                       ._ o o
+                                       \_`-)|_
+                                    ,""       \
+                              -  "  ## |   0 0.
+                                ," ##   ,-\__    `.
+                              ,"       /     `--._;)
+                            ,"     ## /
+                          ,"   ##    /
+
+
+                    """)
+
+        break
